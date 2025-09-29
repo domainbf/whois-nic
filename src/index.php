8405e9a7a707f5e30f70f95de073b3cc6f0bb5e2
不讲李 <158622671+domainbf@users.noreply.github.com>
2025-09-29 22:45:53 +0800

    Update index.php

--- src/index.php
+++ src/index.php
@@ -996,12 +996,16 @@ if ($domain) {
     </button>
   <?php endif; ?>
   <?php if ($parser->remaining): ?>
-    <button class="message-tag message-tag-gray" id="remaining" data-seconds="<?= $parser->remainingSeconds; ?>">
+    <button class="message-tag message-tag-gray message-tag-inline" id="remaining" data-seconds="<?= $parser->remainingSeconds; ?>">
       <svg width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true">
         <path d="M2 1.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1-.5-.5m2.5.5v1a3.5 3.5 0 0 0 1.989 3.158c.533.256 1.011.791 1.011 1.491v.702c0 .7-.478 1.235-1.011 1.491A3.5 3.5 0 0 0 4.5 13v1h7v-1a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351v-.702c0-.7.478-1.235 1.011-1.491A3.5 3.5 0 0 0 11.5 3V2z" />
       </svg>
       <span>距离过期：<?= htmlspecialchars($parser->remaining, ENT_QUOTES, 'UTF-8'); ?></span>
     </button>
+  <?php endif; ?>
+
+  <?php if ($parser->registered): ?>
+    <span class="message-tag message-tag-red registered-status">域名已注册</span>
   <?php endif; ?>
 
   <?php if ($parser->ageSeconds && $parser->ageSeconds < 60 * 24 * 60 * 60): ?>
@@ -1047,8 +1051,8 @@ if ($domain) {
   <?php endif; endif; ?>
 
   <?php if (isset($parser->extension) && in_array(strtolower($parser->extension), ['com', 'net']) && strlen($domainParts[0]) < 6): ?>
-  <span class="message-tag message-tag-green">高流量潜力</span>
-<?php endif; ?>
+    <span class="message-tag message-tag-green">高流量潜力</span>
+  <?php endif; ?>
 
   <?php if ($parser->remainingSeconds < 0 && !$parser->redemptionPeriod): ?>
     <span class="message-tag message-tag-red">过期待续</span>
